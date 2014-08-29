065a1538f1028185b7cf481b32bdba1e2c43dd83
William Blankenship <william.jblankenship@gmail.com>
2014-08-29 13:27:46 -0500

    Auto patch nodewebkit

--- wrapper.js
+++ wrapper.js
@@ -2,6 +2,7 @@
 
 var exec = require('child_process').exec
 var nw = require('nodewebkit').findpath
+var path = require('path')
 if(!process.argv[2]) {
   console.error("Usage: notes FILENAME")
   process.exit(1)
@@ -9,7 +10,21 @@ if(!process.argv[2]) {
 
 console.log(process.cwd())
 exec(nw()+" "+__dirname+" "+process.cwd()+" "+process.argv[2], function(e,stdout,stderr) {
-  if(e) return console.log(e)
   console.log(stdout)
   console.log(stderr)
+  if(e) {
+    if(e.toString().indexOf('libudev')===-1) return console.log(e)
+    console.log("Patching nodewebkit with libudev fix...")
+    return exec(path.join(__dirname,"patchWebkit.sh"), function(e) {
+      console.log(stdout)
+      console.log(stderr)
+      if(e) return console.log(e)
+      console.log("PATCHED!")
+      return exec(nw()+" "+__dirname+" "+process.cwd()+" "+process.argv[2], function(e,stdout,stderr) {
+        console.log(stdout)
+        console.log(stderr)
+        if(e) return console.log(e)
+      })
+    })
+  }
 })
