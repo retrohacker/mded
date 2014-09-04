5784345dccb75213e337977ca0a64741eacda7db
William Blankenship <william.jblankenship@gmail.com>
2014-09-04 09:59:46 -0500

    contents is no longer a property of doc for better or worse

--- doc.js
+++ doc.js
@@ -5,6 +5,7 @@ var fsWorker = require('./fsWorker.js')
 module.exports = doc = new EventEmitter()
 
 var changed = false
+var contents = []
 
 doc.init = function init(file,cb) {
   doc.file = file
@@ -14,19 +15,19 @@ doc.init = function init(file,cb) {
                .replace(/\t/g, '  ')
                .replace(/\u00a0/g, ' ')
                .replace(/\u2424/g, '\n')
-    doc.contents = [data]
+    contents = [data]
     //var lexer = new marked.Lexer()
     /**
     Object.keys(lexer.rules).forEach(function(v) {
       var newContents = []
-      doc.contents.forEach(function(substr) {
+      contents.forEach(function(substr) {
         console.log(substr)
         substr.split(lexer.rules[v]).forEach(function(subsubstr) {
           console.log(subsubstr)
           newContents.push(subsubstr)
         })
       })
-      doc.contents = newContents
+      contents = newContents
       //console.log(lexer.rules[v])
     })
     */
@@ -40,49 +41,54 @@ doc.init = function init(file,cb) {
 segmentFile = function segmentFile() {
   if(!changed) return;
   console.log("Segmenting File!")
-  var data = doc.contents.join('\n')
-  doc.contents = data.split('\n')
+  var data = contents.join('\n')
+  contents = data.split('\n')
   changed = false
   fsWorker.save(data)
 }
 
 doc.getCurrent = function getCurrent() {
-  return doc.contents[doc.index]
+  return contents[doc.index]
 }
 
 doc.getNext = function getNext() {
-  return doc.contents.slice(doc.index+1).join('\n')
+  return contents.slice(doc.index+1).join('\n')
 }
 
 doc.getPrev = function getPrev() {
-  return doc.contents.slice(0,doc.index).join('\n')
+  return contents.slice(0,doc.index).join('\n')
 }
 
-doc.incr = function incr() {
-  // We allow one blank line
-  if(doc.index === doc.contents.length-1) return;
+doc.incr = function incr(val) {
+  val = val || 1
+  if(doc.index+val>contents.length-1) return;
   segmentFile()
-  doc.index++
+  doc.index+=val
   doc.emit("change")
 }
 
-doc.decr = function decr() {
-  if(doc.index === 0) return;
+doc.decr = function decr(val) {
+  val = val || 1
+  if(doc.index-val<0) return;
   segmentFile()
-  doc.index--
+  doc.index-=val
   doc.emit("change")
 }
 
 doc.updateCurrentLine = function updateCurrentLine(string) {
-  fsWorker.save(doc.contents.join('\n'))
-  if(doc.contents[doc.index] == string) return;
-  doc.contents[doc.index] = string
+  fsWorker.save(contents.join('\n'))
+  if(contents[doc.index] == string) return;
+  contents[doc.index] = string
   changed = true
 }
 
 doc.deleteCurrent = function deleteCurrent() {
-  doc.contents = Array.prototype.concat(doc.contents.slice(0,doc.index),doc.contents.slice(doc.index+1))
+  contents = Array.prototype.concat(contents.slice(0,doc.index),contents.slice(doc.index+1))
   doc.index--
   segmentFile()
   doc.emit("change")
+}
+
+doc.rows = function rows() {
+  return contents.length
 }
